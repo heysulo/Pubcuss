b65244843049282103df5d34e52a7b26d007d59a
Sulochana Kodituwakku <sulochana.456@live.com>
2017-05-27 20:13:49 +0530

    Logger function completed

---
+++ app.js
@@ -0,0 +1,45 @@
+/**
+ * Created by sulochana on 5/27/17.
+ */
+
+var moment = require("moment");
+var fs = require('fs');
+
+
+var logstream = fs.createWriteStream("log.txt");
+
+function logger(status,code) {
+    //set default value
+    if (code==undefined){
+        code = 0;
+    }
+
+    //setup time
+    var now = new Date();
+    var content = "";
+    now = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
+
+
+    //switch
+    switch (code){
+        case 0:
+            content = now + " [INFO] " + status;
+            break;
+        case 1:
+            content = now + " [WARN] " + status;
+            break;
+        case 2:
+            content = now + " [CRIT] " + status;
+            break;
+        default:
+            content = now + " [????] " + status;
+            break;
+    }
+
+    //log to file
+    fs.appendFile('log.txt', content+"\n", function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
